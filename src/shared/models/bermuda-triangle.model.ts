8325dbd9b2166cd50cb95662e5557d3c7e5b3a6c
Luan <luanhenriquer8@gmail.com>
2019-05-13 14:38:12 -0300

    adding two models

---
+++ src/shared/models/bermuda-triangle.model.ts
@@ -0,0 +1,16 @@
+import { PointModel } from './point.model';
+
+export class BermudaTriangleModel {
+  public points: PointModel[] = [
+    {
+      latitude: 25.774,
+      longitude: -80.190
+    },{
+      latitude: 18.466,
+      longitude: -66.118
+    },{
+      latitude: 32.321,
+      longitude: -64.757
+    }
+  ];
+}
