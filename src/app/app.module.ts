2fad01e08a8af8d3d21b177eaf3e413531d77900
Luan <luanhenriquer8@gmail.com>
2019-05-13 14:39:47 -0300

    little changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { BermudaTriangleComponent } from './bermuda-triangle/bermuda-triangle.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    BermudaTriangleComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
