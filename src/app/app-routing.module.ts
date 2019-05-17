c17ce32cbcf4b07d2e0968f1365d948f6fe2b584
Luan <luanhenriquer8@gmail.com>
2019-05-17 10:18:02 -0300

    adding the new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
 import { BermudaTriangleComponent } from './bermuda-triangle/bermuda-triangle.component';
+import { SearchOnMapComponent } from './search-on-map/search-on-map.component';
 
 const routes: Routes = [
   { path: '', component: ExampleComponent },
   { path: 'bermuda-triangle', component: BermudaTriangleComponent },
+  { path: 'search-on-map', component: SearchOnMapComponent },
   { path: '', pathMatch: 'full', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
