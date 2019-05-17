d22370e03e24bb84797f792dc21770e175e04a14
Luan <luanhenriquer8@gmail.com>
2019-05-17 13:21:50 -0300

    adding mall route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
 import { BermudaTriangleComponent } from './bermuda-triangle/bermuda-triangle.component';
 import { SearchOnMapComponent } from './search-on-map/search-on-map.component';
+import { MallsOnBrazilFederalDistrictComponent } from './malls-on-brazil-federal-district/malls-on-brazil-federal-district.component';
 
 const routes: Routes = [
   { path: '', component: ExampleComponent },
   { path: 'bermuda-triangle', component: BermudaTriangleComponent },
   { path: 'search-on-map', component: SearchOnMapComponent },
+  { path: 'malls-on-brazil-federal-district', component: MallsOnBrazilFederalDistrictComponent },
   { path: '', pathMatch: 'full', redirectTo: '' }
 ];
 
