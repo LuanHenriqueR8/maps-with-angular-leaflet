5ef6d4e839ef1e435a8c32e8c1b0eab873aa9390
Luan <luanhenriquer8@gmail.com>
2019-05-17 10:17:36 -0300

    importing the new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ExampleComponent } from './example/example.component';
 import { RouterModule } from '@angular/router';
 import { BermudaTriangleComponent } from './bermuda-triangle/bermuda-triangle.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SearchOnMapComponent } from './search-on-map/search-on-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExampleComponent,
-    BermudaTriangleComponent
+    BermudaTriangleComponent,
+    SearchOnMapComponent,
   ],
   imports: [
     BrowserModule,
