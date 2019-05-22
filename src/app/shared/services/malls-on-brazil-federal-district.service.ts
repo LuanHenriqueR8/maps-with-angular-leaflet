ccf6079f236c9407970519429f3bfde6d7aa617a
Luan <luanhenriquer8@gmail.com>
2019-05-22 08:18:16 -0300

    adding mall service

--- src/app/shared/services/malls-on-brazil-federal-district.service.ts
+++ src/app/shared/services/malls-on-brazil-federal-district.service.ts
@@ -6,56 +6,62 @@ import { Mall } from '../models/mall.model';
 })
 export class MallsOnBrazilFederalDistrictService {
 
+  constructor() { }
+
+  public getMallsOnBrazilFederalDistrict(): Mall[] {
+    return this.malls;
+  }
+
   public malls: Mall[] = [
     {
       name: 'Iguatemi Brasilia',
-      latitude: -15.720139,
-      longitude: -47.885445
+      latitude: -15.720211,
+      longitude: -47.886549
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Boulevard Shopping',
+      latitude: -15.718641,
+      longitude: -47.886646
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Brasília Shopping',
+      latitude: -15.786702,
+      longitude: -47.889027
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'ParkShopping Brasília',
+      latitude: -15.833238,
+      longitude: -47.955316
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Conjunto Nacional',
+      latitude: -15.791049,
+      longitude: -47.883133
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Pátio Brasil Shopping',
+      latitude: -15.795928,
+      longitude: -47.892005
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Shopping Pier 21 Paranoá',
+      latitude: -15.817751,
+      longitude: -47.875084
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Felicittá Shopping',
+      latitude: -15.833671,
+      longitude: -48.036994
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Shopping Fashion Mall',
+      latitude: -15.802045,
+      longitude: -47.893958
     },
     {
-      name: '',
-      latitude: 0,
-      longitude: 0
+      name: 'Águas Claras Shopping',
+      latitude: -15.841633,
+      longitude: -48.023771
     },
     {
       name: '',
@@ -73,10 +79,4 @@ export class MallsOnBrazilFederalDistrictService {
       longitude: 0
     }
   ];
-
-  constructor() { }
-
-  public getMallsOnBrazilFederalDistrict(): Mall[] {
-    return this.malls;
-  }
 }
